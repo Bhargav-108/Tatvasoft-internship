ed5ab214660b3eeabc80367a788743648ee80dec
Bhargav Harsoda <bhargavharsoda2003@gmail.com>
2023-05-24 11:51:12 +0530

    task-5

--- src/components/Form1.js
+++ src/components/Form1.js
@@ -1,27 +1,56 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
-import { useState } from "react";
-import Input from "@mui/joy/Input";
+import { useState, useEffect } from "react";
+import TextField from "@mui/material/TextField";
 import Avatar from "@mui/joy/Avatar";
 import Popover from "@mui/material/Popover";
+import { Formik } from "formik";
+import * as Yup from "yup";
 const Form1 = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [open, setOpen] = useState("false");
+  // const [name, setName] = useState();
+  // const [email, setEmail] = useState();
+  const [open, setOpen] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
   const Navigate = useNavigate();
-  const onHomePageButtonClick = () => {
-    console.log("Button Clicked");
-    console.log("Name : ", name);
-    console.log("Email : ", email);
-    Navigate("/");
+
+  useEffect(() => {
+    // console.log("The new value of Name : ", name);
+    // return () => {
+    //   console.log("The old value of Name : ", name);
+    // };
+  }, []);
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "Please make sure you have entered you name with atleast 3 char.")
+      .required("this field is required"),
+    email: Yup.string()
+      .email("Please enter a valid email address")
+      .required("this field is required"),
+    age: Yup.number("age can only be number.")
+      .positive()
+      .integer()
+      .min(15, "minimum age is 15."),
+  });
+
+  const initialValues = {
+    name: "",
+    email: "",
+    age: 15,
+  };
+
+  const onFormSubmit = (values) => {
+    console.log("On the form submitted", values);
+    alert("Form Submmited");
   };
 
   const handleClick = (event) => {
+    console.log(123);
     setAnchorEl(event.currentTarget);
     setOpen(true);
   };
+
   const handleClose = () => {
     setAnchorEl(null);
     setOpen(false);
@@ -32,49 +61,174 @@ const Form1 = () => {
         style={{
           paddingTop: 20,
           display: "flex",
-          justifyContent: "flex-end",
+          flexDirection: "row",
+          justifyContent: "center",
           alignItems: "center",
           columnGap: 5,
         }}
-        onClick={() => setOpen(true)}
+        //onClick={() => setOpen(true)}
       >
-        <Avatar color="primary" variant="solid">
+        {/*<Avatar color="primary" variant="solid">
           BH
-        </Avatar>
-        <span>Bhargav Harsoda</span>
+        </Avatar>*/}
       </div>
-
+      <div style={{ fontSize: "25px", fontWeight: "bolder" }}>
+        Registration Form
+      </div>
       <div
         style={{
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
-          height: "20vh",
-          coloumnGap: "10px",
-          rowGap: "10px",
           padding: "10px",
           margin: "10px",
+          paddingTop: "40px",
         }}
       >
-        <Input
-          type="text"
-          placeholder="Name"
-          variant="outlined"
-          color="neutral"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Input
-          type="email"
-          placeholder="Email"
-          variant="outlined"
-          color="neutral"
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onFormSubmit}
+        >
+          {({
+            value,
+            errors,
+            touched,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginBottom: 5,
+                }}
+              >
+                <div
+                  style={{
+                    textAlign: "left",
+                    fontFamily: "sans-serif",
+                    marginBottom: "10px",
+                    fontSize: "large",
+                  }}
+                >
+                  Name:<span style={{ color: "red" }}>*</span>
+                </div>
+                <TextField
+                  variant="outlined"
+                  type="text"
+                  label="Name"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {touched.name && (
+                  <span
+                    style={{
+                      padding: 5,
+                      color: "red",
+                      fontSize: 16,
+                      fontWeight: 500,
+                    }}
+                  >
+                    {errors.name}
+                  </span>
+                )}
+              </div>
 
-        <Button variant="contained" onClick={onHomePageButtonClick}>
-          Home
-        </Button>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginBottom: 5,
+                }}
+              >
+                <div
+                  style={{
+                    textAlign: "left",
+                    fontFamily: "sans-serif",
+                    marginBottom: "10px",
+                    fontSize: "large",
+                  }}
+                >
+                  Email:<span style={{ color: "red" }}>*</span>
+                </div>
+                <TextField
+                  variant="outlined"
+                  type="email"
+                  label="Email"
+                  id="email"
+                  name="email"
+                  placeholder="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {touched.email && (
+                  <span
+                    style={{
+                      padding: 5,
+                      color: "red",
+                      fontSize: 16,
+                      fontWeight: 500,
+                    }}
+                  >
+                    {errors.email}
+                  </span>
+                )}
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginBottom: 5,
+                }}
+              >
+                <div
+                  style={{
+                    textAlign: "left",
+                    fontFamily: "sans-serif",
+                    marginBottom: "10px",
+                    fontSize: "large",
+                  }}
+                >
+                  Age:
+                </div>
+                <TextField
+                  variant="outlined"
+                  type="number"
+                  label="Age"
+                  id="age"
+                  name="age"
+                  placeholder="age"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {touched.age && (
+                  <span
+                    style={{
+                      padding: 5,
+                      color: "red",
+                      fontSize: 16,
+                      fontWeight: 500,
+                    }}
+                  >
+                    {errors.age}
+                  </span>
+                )}
+              </div>
+              <Button variant="contained" type="submit" className="">
+                Submit
+              </Button>
+            </form>
+          )}
+        </Formik>
       </div>
+      {/*
       <Popover
         open={open}
         anchorOrigin={{
@@ -94,6 +248,7 @@ const Form1 = () => {
           </Button>
         </div>
       </Popover>
+      */}
     </div>
   );
 };
